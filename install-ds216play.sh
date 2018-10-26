77ba726b7e1f8fdbf61083d3414458d31be2e359
Matthias Hausherr <matthias.hausherr@ti8m.ch>
2018-10-26 07:20:50 +0200

    Simplified threshold

--- install-ds216play.sh
+++ install-ds216play.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 env GOOS=linux GOARCH=386 go build -o $GOBIN/directory-monitor-linux-386
-mv $GOBIN/directory-monitor-linux-386 ~/Dropbox/tmp/
-cp -r /home/mha/.go/ ~/Dropbox/tmp/
+cp $GOBIN/directory-monitor-linux-386 ~/Dropbox/tmp/
+cp -r /home/mha/.go/* ~/Dropbox/tmp/
